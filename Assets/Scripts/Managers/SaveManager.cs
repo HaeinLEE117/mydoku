c2809cd85d176d9131c2a763f06f0a79718f2051
HaeinLEE117 <heyin0122@korea.ac.kr>
2026-07-03 16:02:17 +0900

    [Remove] 검증용 로그 삭제

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -11,10 +11,6 @@ public class SaveManager
         string jsonData = JsonUtility.ToJson(data);
         PlayerPrefs.SetString(SAVE_KEY, jsonData);
         PlayerPrefs.Save();
-        Debug.Log("Game data saved successfully.");
-        Debug.Log("PlayerPrefs 저장 위치: HKEY_CURRENT_USER\\Software\\" +
-          Application.companyName + "\\" + Application.productName);
-
     }
 
     public static PlayerData LoadPlayerData()
@@ -23,12 +19,10 @@ public class SaveManager
         {
             string jsonData = PlayerPrefs.GetString(SAVE_KEY);
             PlayerData data = JsonUtility.FromJson<PlayerData>(jsonData);
-            Debug.Log($"Game data loaded. Current Level: {data.currentLevel}");
             return data;
         }
         else
         {
-            Debug.Log("No save data found. Creating new player data.");
             PlayerData pd = new PlayerData();
             SavePlayerData(pd);
             return pd;
@@ -41,7 +35,6 @@ public class SaveManager
         {
             PlayerPrefs.DeleteKey(SAVE_KEY);
             PlayerPrefs.Save();
-            Debug.Log("Save data deleted.");
         }
     }
 
